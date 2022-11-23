9d03f2b976b824b4e2b00d2eab798a707c8561f2
sunil880018 <sunil880018@gmail.com>
2022-11-23 08:15:13 +0530

    adding hhtp

--- main.go
+++ main.go
@@ -1,26 +1,28 @@
 package main
 
 import "github.com/gin-gonic/gin"
+import "net/http"
 
 type Person struct{
-	Name string;
-	Age int
+	Name string `json:"name"`
+	Age int `json:"age"`
 }
 
 func main() {
 	r := gin.Default() // Creates a gin router with default middleware:
 	                   // Default With the Logger and Recovery middleware already attached
 					   
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/ping", func(c *gin.Context) { 
 
 		var p Person // Person Object
 
-		p.Name = "Sunil Kumar";
+		p.Name = "Sunil Kumar"; // Note that p.Name becomes "name" in the JSON
 		p.Age = 24;
 
 		c.Bind(&p)
          
-		c.JSON(200, gin.H{
+		// c.JSON() return output in json format
+		c.JSON(http.StatusOK, gin.H{ // gin.H is a shortcut for map[string]interface{}
 			"name": p.Name,
 			"Age":p.Age,
 		})
